81cab846ee19e16d7cf3d080345d3a60c1e86911
schottmueller <schottmueller@users.noreply.github.com>
2019-12-16 16:15:21 +0100

    Update GoogleMaps.jl
    delete the following:    
    include("directions.jl")
        include("nearby.jl")
        include("search.jl")
        include("details.jl")
        include("photos.jl")
        include("matchroads.jl")

--- src/GoogleMaps.jl
+++ src/GoogleMaps.jl
@@ -1,14 +1,6 @@
 module GoogleMaps
 
     import JSON, HTTP, ImageMagick
-
     include("utils.jl")
-    include("directions.jl")
     include("geocode.jl")
-    include("nearby.jl")
-    include("search.jl")
-    include("details.jl")
-    include("photos.jl")
-    include("matchroads.jl")
-
 end
